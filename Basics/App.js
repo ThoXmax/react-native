de2caa20b46625f7706482b20479ad933b292be2
max <85299566+ThoXmax@users.noreply.github.com>
2021-08-23 10:23:53 +0800

    feat: add SectionList Basics

--- Basics/App.js
+++ Basics/App.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { Text, View, TextInput, Image, Button, ScrollView, FlatList, StyleSheet } from 'react-native';
+import { Text, View, TextInput, Image, Button, ScrollView, FlatList, StyleSheet, SectionList } from 'react-native';
 
 // React Fundamentals
 // Component:
@@ -274,4 +274,48 @@ const FlatListBasics = () => {
   );
 }
 
-export default FlatListBasics;
+//export default FlatListBasics;
+
+
+// ==================================================
+// import React from 'react';
+// import { SectionList, StyleSheet, Text, View } from 'react-native';
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247, 247, 247, 1.0)'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  }
+})
+
+const SectionListBasics = () => {
+  return (
+    <View style={styles1.container}>
+      <SectionList 
+        sections={[
+          {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+          {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']}
+        ]}
+        renderItem={({item}) => <Text style={styles1.item}>{item}</Text>}
+        renderSectionHeader={({section}) => <Text style={styles1.sectionHeader}>{section.title}</Text>}
+        keyExtractor={(item, index) => index}
+      />
+    </View>
+  )
+}
+
+export default SectionListBasics;
