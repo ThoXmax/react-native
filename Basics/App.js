5363833b81648c34ec430caaed7ff30d4ed51da0
max <85299566+ThoXmax@users.noreply.github.com>
2021-08-23 10:00:55 +0800

    feat: add FlatList Basics

--- Basics/App.js
+++ Basics/App.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { Text, View, TextInput, Image, Button, ScrollView } from 'react-native';
+import { Text, View, TextInput, Image, Button, ScrollView, FlatList, StyleSheet } from 'react-native';
 
 // React Fundamentals
 // Component:
@@ -233,3 +233,45 @@ const App = () => (
 );
 
 // export default App;
+
+
+// ==================================================
+// FlatList Basics:
+// import React from 'react';
+// import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  }
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'}                                                                                                                                                        
+        ]}
+        renderItem = {({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+}
+
+export default FlatListBasics;
