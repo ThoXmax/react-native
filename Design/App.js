89dd88271f7e1939947c1090b5c1e836c47ee8da
max <85299566+ThoXmax@users.noreply.github.com>
2021-08-25 14:23:54 +0800

    feat: add flex example(style)

--- Design/App.js
+++ Design/App.js
@@ -98,4 +98,32 @@ const PercentageDimensionsBasics = () => {
   );
 };
 
-export default PercentageDimensionsBasics;
+// export default PercentageDimensionsBasics;
+
+
+// ==================================================
+// Layout with Flexbox:
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+
+const Flex = () => {
+  return (
+    <View style = {[Styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 1, backgroundColor: "red" }} />
+      <View style={{ flex: 2, backgroundColor: "darkorange" }}/>
+      <View style={{ flex: 3, backgroundColor: "green"}} />
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    padding: 20
+  }
+});
+
+export default Flex;
